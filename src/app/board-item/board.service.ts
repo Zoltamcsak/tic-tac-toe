cd6119b280dbdaf0369ac3d71108982744a61a99
Tamas Zolcsak <tamas.zolcsak@adnovum.hu>
2017-03-25 10:14:16 +0100

    -created computer's move

--- src/app/board-item/board.service.ts
+++ src/app/board-item/board.service.ts
@@ -2,11 +2,14 @@ import {Injectable} from '@angular/core';
 import {BoardItem} from './board-item';
 import {ItemState} from './item-state';
 import {GameState} from '../game/game-state';
+import {takeUntil} from 'rxjs/operator/takeUntil';
 @Injectable()
 export class MatrixService {
   public items: BoardItem[][] = [];
   public gameState: GameState;
-  public boardLines = [];
+  public boardLines: any[];
+  public won: boolean;
+  public draw: boolean;
 
   public init(): void {
     this.gameState = GameState.X_TURN;
@@ -17,19 +20,39 @@ export class MatrixService {
         this.items[i].push(item);
       }
     }
+    this.boardLines = [];
     this.setBoardLines();
+    this.won = false;
+    this.draw = false;
   }
 
   public updateBoard(item: BoardItem): void {
-    item.state = this.gameState === GameState.X_TURN ? ItemState.X : ItemState.O;
+    if (this.gameState === GameState.X_TURN) {
+      item.state = this.getNextState();
+    }
     this.updateGameState();
+    setTimeout(() => {
+      this.computerMove()
+    }, 1000)
+  }
+
+  public getNextState(): ItemState {
+    return this.gameState === GameState.X_TURN ? ItemState.X : ItemState.O;
   }
 
   updateGameState(): void {
-    if (this.gameState === GameState.X_TURN) {
-      this.gameState = GameState.O_TURN;
+    let winningPlayer = this.isWinning();
+    if (winningPlayer === ItemState.NOTHING) {
+      if (this.isDraw()) {
+        this.draw = true;
+      } else if (this.gameState === GameState.X_TURN) {
+        this.gameState = GameState.O_TURN;
+      } else {
+        this.gameState = GameState.X_TURN;
+      }
     } else {
-      this.gameState = GameState.X_TURN;
+      this.won = true;
+      this.gameState = winningPlayer === ItemState.X ? GameState.X_WON : GameState.O_WON;
     }
   }
 
@@ -52,5 +75,39 @@ export class MatrixService {
     this.boardLines.push(secondaryDiagonal);
   }
 
+  isWinning(): ItemState {
+    let resultState = ItemState.NOTHING;
+    this.boardLines.map((boardLine: BoardItem[]) => {
+      if (boardLine[0].state !== ItemState.NOTHING &&
+          boardLine[0].state === boardLine[1].state &&
+          boardLine[0].state === boardLine[2].state &&
+          boardLine[1].state === boardLine[2].state) {
+        resultState = boardLine[0].state;
+      }
+    });
+    return resultState;
+  }
 
+  isDraw(): boolean {
+    for (let i=0; i<3; i++) {
+      let state = this.items[i].every((item: BoardItem) => item.state !== ItemState.NOTHING);
+      if (!state) return false;
+    }
+    return true;
+  }
+
+  computerMove(): void {
+    if (this.gameState !== GameState.O_TURN) {
+      return;
+    }
+    let nextMove;
+    for (let i=0; i<3; i++) {
+      nextMove = this.items[i].find(item => item.state === ItemState.NOTHING);
+      if (nextMove) {
+        break;
+      }
+    }
+    nextMove.state = this.getNextState();
+    this.updateGameState();
+  }
 }
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,16 +1,21 @@
 import {Component, OnInit} from '@angular/core';
 import {BoardItem} from '../board-item/board-item';
 import {MatrixService} from '../board-item/board.service';
+import {GameState} from './game-state';
 @Component({
   selector: 'app-game',
   template: `
-      <div class='item-row' *ngFor='let row of this.matrixService.items'>
-          <app-board-item *ngFor='let col of row' [state]='col.state' (itemClick)='onItemClick(col)'></app-board-item>
-      </div>`,
+    <button class='btn btn-primary' (click)='onNewGameClicked()'>New game</button>
+    <div *ngIf='matrixService.won'>Player {{matrixService.gameState}} won</div>
+    <div *ngIf='matrixService.draw'>It's a draw</div>
+    <div class='item-row' *ngFor='let row of this.matrixService.items'>
+        <app-board-item *ngFor='let col of row' [state]='col.state' (itemClick)='onItemClick(col)'></app-board-item>
+    </div>`,
   styleUrls: ['game.scss']
 })
 export class GameComponent implements OnInit {
   nrOfRows: number;
+  private nextTurn: GameState;
   constructor(public matrixService: MatrixService) {}
 
   ngOnInit(): void {
@@ -18,6 +23,16 @@ export class GameComponent implements OnInit {
   }
 
   onItemClick(item: BoardItem): void {
-    this.matrixService.updateBoard(item);
+    if (this.isPlayersMove()) {
+      this.matrixService.updateBoard(item);
+    }
+  }
+
+  onNewGameClicked(): void {
+    this.matrixService.init();
+  }
+
+  isPlayersMove(): boolean {
+    return this.matrixService.gameState === GameState.X_TURN;
   }
 }
